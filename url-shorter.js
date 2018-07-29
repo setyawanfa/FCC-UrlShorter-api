4920ff9dbd4bda1e9563d6051259ca12c4a38d11
Fajar Andy Setiawan <fajar.andy@xtremax.com>
2018-07-29 19:17:25 +0700

    add redirect on get api call

--- url-shorter.js
+++ url-shorter.js
@@ -1,8 +1,21 @@
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 var validUrl = require('valid-url');
 const dns = require('dns');
+var Schema = mongoose.Schema;
+var schema = new Schema({
+    url : String,
+    identifier : Number
+    });
+
+var urlStore = mongoose.model('urllist',schema);
 module.exports = function(app){
     //console.log(process.env.MONGO_URI);
+    //create schema to store db
+    //var Schema = mongoose.Schema;
+    
+
+    
+
     mongoose.connect(process.env.MONGO_URI,{ useMongoClient: true },function(err){
         if(err){
             console.log("There is some problem with the connection" + err);
@@ -11,23 +24,38 @@ module.exports = function(app){
             console.log("Connection run successfully");
         }
         
+
+
         function pqueryhandler(req,res){
-            var url = req.body.url;
-            if (!validUrl.isUri(url))
+            var uri = req.body.url;
+            if (!validUrl.isUri(uri))
                 res.json({"error":"invalid URL"});
             else{
                 console.log("Url is valid");
-                dns.lookup(extractHostname(url),function(err,address,family){
+                dns.lookup(extractHostname(uri),function(err,address,family){
                     console.log(address);
                     if(address!= undefined)
-                        res.json({"DNS existed" :  url});
+                    {
+                        
+                        var val = Math.floor(1000 + Math.random() * 9000);
+                        
+                        var urltostore = new urlStore({url : uri,identifier : val});
+                        
+                        urltostore.save(function(err,data){
+                            if(err){
+                                console.log("Error on storing data " + err);
+                            }
+                            else{
+                                res.json({"original_url":uri, "short_url" :val});
+                            }
+                        })
+                    }
+                        //res.json({"DNS existed" :  url});
                     else{
-                        res.json({"DNS is false": url});
+                        res.json({"DNS is false": uri});
                     }
                 })
-            }
-            
-            
+            }                        
           }
 
           function extractHostname(url) {
@@ -48,6 +76,38 @@ module.exports = function(app){
         
             return hostname;
         }
+
+        function gqueryhandle(req,res){
+            let lnk = req.params.shortlink;
+
+            urlStore.findOne({identifier : lnk},function(err,obj){
+                if(err)
+                    console.log("Error in retrieving data " + err);
+                else{
+                    if(obj == null){
+                        res.json({"short url" : "not found"});
+                    }
+                    else{
+                        //res.json({lnk : "found", "redirection" : obj.url});
+                        
+                        
+                        res.redirect(obj.url);
+                        
+                    }
+                }
+            })
+        }
+
+        /* function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+          }
+          
+        async function demo() {
+            await sleep(500);
+        } */
+
+
         app.route("/api/shorturl/new").post(pqueryhandler)
+        app.route("/api/shorturl/:shortlink").get(gqueryhandle)
     })   
 }
